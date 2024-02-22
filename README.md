35595e29385364b8aa3011b59a27f6d0acdb7fed
patelshehbaz <patelshehbaz27@gmail.com>
2024-02-22 15:54:17 -0500

    project structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # US-Visa-Approval-Prediction
+
+# How to run?
+
+```
+conda create -n visa python=3.8 -y
+```
+
+```
+conda activate hate
+```
+
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shehbaz",
+    author_email="patelshehbaz27@gmail.com",
+    packages=find_packages()
+)
