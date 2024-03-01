80f6b577e1c7be29d6a939c305096f8e51c4e9cc
patelshehbaz <patelshehbaz27@gmail.com>
2024-03-01 11:39:30 -0500

    Model training

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -40,3 +40,10 @@ class DataTransformationConfig:
                                                      DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                      PREPROCSSING_OBJECT_FILE_NAME)
 
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR_NAME)
+    trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
+    expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
+    model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
+
